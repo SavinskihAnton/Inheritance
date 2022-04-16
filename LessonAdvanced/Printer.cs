3521159652dad8b0f9c107bd1dcd5a164c65309e
SavinskihAnton <102802345+SavinskihAnton@users.noreply.github.com>
2022-04-16 14:56:09 +0700

    Добавьте файлы проекта.

---
+++ LessonAdvanced/Printer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonAdvanced
+{
+    class Printer
+    {
+        protected ConsoleColor color;
+        public Printer(ConsoleColor color)
+        {
+            this.color = color;
+        }
+        public virtual void Print(string value)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+            Console.ForegroundColor= ConsoleColor.Gray;
+        }
+    }
+    class ColorPrinter : Printer
+    {
+        public ColorPrinter(ConsoleColor color)
+            : base(color)
+        {
+
+        }
+    }
+}
---
+++ LessonAdvanced/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задание
+//Используя Visual Studio, создайте проект по шаблону Console Application.
+//Требуется:
+//Создайте класс Printer.
+//В теле класса создайте метод void Print(string value), который выводит на экран значение
+//аргумента.
+//Реализуйте возможность того, чтобы в случае наследования от данного класса других классов, и вызове
+//соответствующего метода их экземпляра, строки, переданные в качестве аргументов методов,
+//выводились разными цветами.
+//Обязательно используйте приведение типов.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonAdvanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ColorPrinter color = new ColorPrinter(ConsoleColor.DarkGreen);
+            color.Print("Green");
+
+            Printer printUp = color;
+            printUp.Print("Green");
+
+            ColorPrinter color1 = new ColorPrinter(ConsoleColor.Red);
+            color1.Print("Red");
+
+            Console.ReadKey();
+        }
+    }
+}
